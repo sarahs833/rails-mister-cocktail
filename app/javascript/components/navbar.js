a6ec72f057ba01ba50e3db491458b5cc69d06022
sarah wang <sarahwang47@gmail.com>
2020-01-09 12:43:10 +0800

    new file

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -18,11 +18,23 @@ const initUpdateNavbarOnScroll = () => {
 
 const toggle = () => {
   const icon = document.querySelector('#nav-icon span');
+  const iconBlack = document.querySelector('#icon-black span');
+  const hi = document.querySelector('hi');
   const bigDiv = document.querySelector('.navbar');
+  const overlay = document.querySelector('.overlay-nav-open')
   icon.addEventListener('click',switchVisible);
   function switchVisible(event) {
     bigDiv.classList.remove('navbar');
     bigDiv.classList.add('overlay-nav-open');
+    document.getElementById('nav-icon').id = "icon-black";
+    document.querySelector('.linear').classList.add('switched')
+    // hi.classList.add('icon-black');
+  }
+  iconBlack.addEventListener('click',switchHide);
+   function switchHide(){
+     overlay.classList.remove('overlay-nav-open');
+     overlay.classList.add('navbar');
+     document.getElementById('icon-black').id = "nav-icon"
   }
 }
 
